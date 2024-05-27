e96112a7bf9eaa95b52814d409d392754ed7d3d8
SheviEngelsberg <s4161823@gmail.com>
2024-05-28 02:15:05 +0300

    fix expense_router

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import uvicorn as uvicorn
 from fastapi import FastAPI
 from app.controllers.user_controller import user_router
-from app.controllers.expense_controller import expenses_router
+from app.controllers.expense_controller import expense_router
 from app.controllers.revenue_controller import revenue_router
+from app.visualization.graph_router import graph_router
 
 app = FastAPI()
 app.include_router(user_router, prefix='/user')
-app.include_router(expenses_router, prefix='/expense')
+app.include_router(expense_router, prefix='/expense')
 app.include_router(revenue_router, prefix='/revenue')
+app.include_router(graph_router, prefix='/visualization')
 
 
 if __name__ == '__main__':
