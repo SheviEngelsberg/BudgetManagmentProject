64926e8df7a05dcf204a9f1b06cb3a935be7cb35
SheviEngelsberg <s4161823@gmail.com>
2024-05-29 11:09:52 +0300

    Remove folder from repository

--- app/main.py
+++ app/main.py
@@ -13,5 +13,5 @@ app.include_router(visualization_router, prefix='/visualization')
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8002)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
